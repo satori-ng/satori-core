0cc2c578c02d37525b8f1f69f788ea224f82cb25
Dimitris Zervas <dzervas@dzervas.gr>
2018-04-05 14:59:51 +0300

    :shirt: PEP8!

--- satoricore/image.py
+++ satoricore/image.py
@@ -40,6 +40,7 @@ _STANDARD_EXT = [
 class FileNotFoundError(Exception):
     pass
 
+
 class NotADirectoryError(Exception):
     pass
 
@@ -68,21 +69,26 @@ class SatoriImage(object):
         self.__data['metadata']['system']['processor'] = platform.processor()
         self.__data['metadata']['system']['specifics'] = {}
         # try :
-        #     self.__data['metadata']['system']['specifics']['win'] = platform.win32_ver()
+        #     self.__data['metadata']['system']['specifics']['win'] = \
+        #        platform.win32_ver()
         # except :
         #     pass
         # try :
-        #     self.__data['metadata']['system']['specifics']['mac'] = platform.mac_ver()
+        #     self.__data['metadata']['system']['specifics']['mac'] = \
+        #        platform.mac_ver()
         # except :
         #     pass
 
     def add_file(self, full_path, type_=_FILE_T):
-        filedict = self.set_attribute(full_path, {}, _CONTENTS_S, force_create=True)
+        filedict = self.set_attribute(full_path, {},
+                                      _CONTENTS_S, force_create=True)
         filedict[_TYPE_S] = type_
 
-    def set_attribute(self, full_path, attr_dict, ext_name, force_create=False):
+    def set_attribute(self, full_path, attr_dict,
+                      ext_name, force_create=False):
         """
-        Adds an attribute to a file specified in the 'full_path', and creates it if it doesn't exist.
+        Adds an attribute to a file specified in the 'full_path'
+        and creates it if it doesn't exist.
         """
         file_dict = self.__get_file_dict(full_path, force_create=force_create)
         if ext_name not in _STANDARD_EXT:
@@ -93,7 +99,8 @@ class SatoriImage(object):
     def __get_file_dict(self, full_path, force_create=False):
         """
         Returns the 'dict' object for the 'full_path' specified.
-        If 'force_create' is enabled and the 'full_path' does not exist, a dict is automatically created for that path
+        If 'force_create' is enabled and the 'full_path' does not exist,
+        a dict is automatically created for that path
         """
         # Eliminate trailing '/' to make splitting easier
         if full_path.endswith(os.sep):
@@ -108,7 +115,9 @@ class SatoriImage(object):
                 try:
                     # Try Accessing directory contents
                     if _CONTENTS_S not in cur_position[token].keys():
-                        raise NotADirectoryError("File: '{}' in Path: '{}' is not a Directory".format(token, full_path))
+                        raise NotADirectoryError(
+                                "File: '{}' in Path: '{}' is not a Directory"
+                                .format(token, full_path))
                     # cur_position[token][_CONTENTS_S]
                 except KeyError:
                     # Directory doesn't exist - create it
@@ -136,7 +145,6 @@ class SatoriImage(object):
             pprint(self.__data[key])
         else:
             pprint(self.__data)
-
 
 
 if __name__ == '__main__':
