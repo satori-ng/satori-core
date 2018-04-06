0eef0d4be3fac5300be5d2ee2e3fcd96828d3291
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2018-04-06 16:50:25 +0300

    Add UnixCrawler docstring

--- satoricore/crawler/unixcrawler.py
+++ satoricore/crawler/unixcrawler.py
@@ -2,6 +2,15 @@ from .basecrawler import BaseCrawler
 
 
 class UnixCrawler(BaseCrawler):
+    """
+    Like BaseCrawler, performs filesystem crawling excluding Unix specific
+    paths that contain temporary files that may disrupt the iteration of the
+    filesystem. The following paths are excluded:
+        /proc
+        /sys
+        /dev
+        /tmp
+    """
     def __init__(self, entrypoints, excluded_dirs=set()):
         excluded_dirs.union([
             '/proc',
