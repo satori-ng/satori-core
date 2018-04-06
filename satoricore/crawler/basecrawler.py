8c91d63ba938869aa2019a97816e0bc46afcaea4
Zero Pixel <5311065+zer0pixel@users.noreply.github.com>
2018-04-06 16:28:59 +0300

    Split __call__ in BaseCrawler into two  functions

--- satoricore/crawler/basecrawler.py
+++ satoricore/crawler/basecrawler.py
@@ -54,13 +54,12 @@ class BaseCrawler:
         self.entrypoints = entrypoints
         self.excluded_dirs = excluded_dirs
 
-    def __call__(self):
+    def _iter_entrypoints(self):
         for entrypoint in self.entrypoints:
             # Iterate over the list from top top bottom so that we may edit the
             # list of directories to be traversed according to the list of
             # excluded dirs.
             for _root, _dirs, _files in os.walk(entrypoint, topdown=True):
-
                 root = os.path.abspath(_root)
                 # TODO: This is most probably not needed. Remove after further
                 # testing.
@@ -78,12 +77,17 @@ class BaseCrawler:
                 dirs = [os.path.join(root, d) for d in _dirs]
                 files = [os.path.join(root, f) for f in _files]
 
-                for _dir in dirs:
-                    dir_stat = os.lstat(_dir)
-                    yield (_dir, _DIRECTORY_T, dir_stat)
+                yield (dirs, files)
 
-                for _file in files:
-                    file_stat = os.lstat(_file)
-                    mode = stat.S_IFMT(file_stat.st_mode)
-                    _type = st_mode_mapper.get(mode, _UNKNOWN_T)
-                    yield (_file, _type, file_stat)
+    def __call__(self):
+        for dirs, files in self._iter_entrypoints():
+
+            for _dir in dirs:
+                dir_stat = os.lstat(_dir)
+                yield (_dir, _DIRECTORY_T, dir_stat)
+
+            for _file in files:
+                file_stat = os.lstat(_file)
+                mode = stat.S_IFMT(file_stat.st_mode)
+                _type = st_mode_mapper.get(mode, _UNKNOWN_T)
+                yield (_file, _type, file_stat)
