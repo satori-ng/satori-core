0b9c66f4d902838895305b83712dce05c7935c00
operatorequals <john.torakis@gmail.com>
2018-04-15 19:11:18 +0300

    Fixed the magic extension (resource cleaning), removed not used extension

--- extensions/magic_ext.py
+++ extensions/magic_ext.py
@@ -6,10 +6,17 @@ from satoricore.hooker import hook
 __name__ = 'mime'
 
 
+magic_obj = magic.Magic(flags=magic.MAGIC_MIME_TYPE)
+
 @hook("with_open")
-def hash_file(satori_image, file_path, file_type, fd):
+def mime_file(satori_image, file_path, file_type, fd):
     fd.seek(0)
-    magic_obj = magic.Magic(flags=magic.MAGIC_MIME_TYPE)
     chunk = fd.read(1024)
     mime = magic_obj.id_buffer(chunk)
     satori_image.set_attribute(file_path, mime, __name__, force_create=True)
+
+
+
+@hook("on_end")
+def clean_magic():
+    magic_obj.close()
