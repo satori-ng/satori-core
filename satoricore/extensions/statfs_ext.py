ba0c5bda19ea6d1aecd0d3c15ca3739979d89aaa
operatorequals <john.torakis@gmail.com>
2018-04-22 00:39:21 +0300

    Fixed statfs_ext to return when finding symlink

--- satoricore/extensions/statfs_ext.py
+++ satoricore/extensions/statfs_ext.py
@@ -1,12 +1,20 @@
 import os
+
 from hooker import hook
 
 
 @hook("pre_open")
 def get_statfs_info(satori_image, file_path, file_type):
+    # pass
+    if os.path.islink(file_path):
+        return
     file_stat = os.statvfs(file_path)
 
     # Translates statvfs' attributes to a dict
-    stat_dict = {x[2:]: getattr(file_stat, x) for x in dir(file_stat) if x.startswith("f_")}
+    stat_dict = {x[2:]:
+        getattr(file_stat, x)
+        for x in dir(file_stat)
+        if x.startswith("f_")
+        }
 
     satori_image.set_attribute(file_path, stat_dict, 'statfs', force_create=True)
