55e92ba043048c4b32cbfedfe7dbd02d76856c89
operatorequals <john.torakis@gmail.com>
2018-05-14 17:29:04 +0300

    Added method to get SatoriContext from given string, utilizing satoriremote if available

--- satoricore/common.py
+++ satoricore/common.py
@@ -1,3 +1,8 @@
+from contextlib import contextmanager
+import os
+from satoricore.logger import logger
+
+
 class _STANDARD_EXT(object):
     DIRECTORY_T = 'D'
     FILE_T = 'F'
@@ -7,3 +12,44 @@ class _STANDARD_EXT(object):
     FIFO_T = 'I'
     SOCKET_T = 'S'
     UNKNOWN_T = 'U'
+
+
+
+@contextmanager
+def dummy_context(obj):
+    yield obj
+
+def get_image_context_from_arg(arg, allow_local=True):
+
+    if arg == '.':
+        return dummy_context(os)
+
+    try:
+        os.stat(arg)
+        logger.info("Found to '{}'".format(arg))
+
+        image_path = arg
+        source = load_image(image_path)
+        # print (arg)
+        if source != None:
+            return dummy_context(source)
+    except FileNotFoundError:
+        pass
+
+    try:
+        import satoriremote
+        logger.info("Connecting to '{}'".format(arg))
+        conn_context_source, conn_dict = satoriremote.connect(arg)
+        logger.info("[+] Connected to {}".format(
+                            conn_dict['host']
+                        )
+                    )
+        return conn_context_source
+        # with conn_context_source as context:
+        #     return context
+    except ImportError:
+        logger.warn("'satoriremote' package not available, remote paths can't be used")
+    except ValueError:  # If can't be parsed as regular expression
+        pass
+    except ConnectionError:
+        logger.warn("Connection failed for path '{}'".format(arg))
