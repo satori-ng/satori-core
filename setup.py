8e6c93e1b15d128f061b830036e811032f9503a0
operatorequals <john.torakis@gmail.com>
2018-05-14 17:55:10 +0300

    The correct 'setup.py'

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup
 
-import satoriremote
+import satoricore
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name=satoriremote.__name__,
-    description=satoriremote.__desc__,
-    version=satoriremote.__version__,
+    name=satoricore.__name__,
+    description=satoricore.__desc__,
+    version=satoricore.__version__,
 
     author="Satori-NG org",
-    author_email=satoriremote.__email__,
+    author_email=satoricore.__email__,
 
-    packages=["satoriremote"],
+    packages=["satoricore"],
 
     entry_points={
         "console_scripts": [
-            "satori-remote=satoriremote.__main__:main",
+            "satori-file=satoricore.file.__main__:main",
         ],
     },
     install_requires=requirements,
 
 )
 
-# python -m unittest discover tests
+
